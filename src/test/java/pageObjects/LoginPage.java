fafc81b4835ca65c9eae80abc45da929ef501525
drgmircea <drgmircea23@gmail.com>
2024-03-06 13:45:38 +0200

    Update LoginPage.java

--- src/test/java/pageObjects/LoginPage.java
+++ src/test/java/pageObjects/LoginPage.java
@@ -21,8 +21,27 @@ public class LogInPage extends BasePage {
     public void clickLogin() {
         Login.click();
     }
-    @FindBy(xpath="//a[contains(text(),'Creează-ți cont folosind Email')]")
-    WebElement create_cont;
-    public void ClickCreate_cont() {
-        create_cont.click();
+   
+    @FindBy(xpath="//input[@placeholder='Parolă']")
+    WebElement txtPassword;
+    public void password(String pass) {
+        txtPassword.sendKeys(pass);
     }
+    @FindBy(xpath="//div[@class='right']//div//a[@class='link'][normalize-space()='Meniu']")
+    WebElement menu;
+    public void clickmenu() {
+        menu.click();
+    }
+
+    @FindBy(xpath="//li[5]//div[1]//div[1]//div[1]//img[1]")
+    WebElement gustari;
+    public void aleggustari() {
+        gustari.click();
+    }
+
+    @FindBy(xpath="//span[contains(text(),'Cartofi prăjiți cu crustă crocantă - 170 gr')]")
+    WebElement cartofiselect;
+    public void cartofi() {
+        cartofiselect.click();
+    }
+}
