1a6798d220322e404612703e6bd721816a2f5bd3
drgmircea <drgmircea23@gmail.com>
2024-03-06 12:15:38 +0200

    Create HomePage.java

---
+++ src/test/java/pageObjects/HomePage.java
@@ -0,0 +1,27 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage extends BasePage{
+
+	WebDriver driver;
+	
+	public HomePage(WebDriver driver)
+	{
+		super(driver);
+	}
+
+	@FindBy(xpath=" //a[normalize-space()='Log in']")
+		WebElement lnkLogin;
+
+	public void lnkLogin() { lnkLogin.click(); }
+	@FindBy(xpath="//div[@class='right']//a[@class='link'][normalize-space()='Cont']")
+	WebElement lnkCont;
+	public void clickCont()
+		{
+		lnkCont.click();
+		}
+
+}
