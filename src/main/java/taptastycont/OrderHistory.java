77ce49774e10d6b3c095122490942f02b0114a4d
alexmorja <alexmorja@gmail.com>
2024-03-14 19:33:32 +0200

    Add to Cont tests, check OrderHistory test and back to account

---
+++ src/main/java/taptastycont/OrderHistory.java
@@ -0,0 +1,33 @@
+package taptastycont;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+public class OrderHistory {
+
+    By accountButton = By.xpath("(//div/a[@class='link'])[3]");
+    By historyOrderButton = By.xpath("(//div[@class = 'item-title'])[3]");
+    By backButton = By.xpath("//i[@class= 'icon f7-icons']");
+
+    public WebDriver driver;
+    public WebDriverWait wait;
+
+    public OrderHistory(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+    }
+
+    public void orderHistory() throws InterruptedException {
+        TimeUnit.SECONDS.sleep(2);
+        driver.findElement(accountButton).click();
+        TimeUnit.SECONDS.sleep(1);
+        driver.findElement(historyOrderButton).click();
+        wait.until(ExpectedConditions.elementToBeClickable(backButton));
+        driver.findElement(backButton).click();
+    }
+}
---
+++ src/test/java/taptastytestcont/OrderHistoryTest.java
@@ -0,0 +1,39 @@
+package taptastytestcont;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import taptasty.LoginPage;
+import taptastycont.OrderHistory;
+import utils.BasePage;
+
+//Check order history and go back to account
+public class OrderHistoryTest extends BasePage {
+
+    @Test
+    public void orderHistory () throws  InterruptedException {
+
+        logger.info("**** starting TC_OrderHistory  *****");
+
+        logger.debug("application logs started......");
+
+        try {
+
+        driver.get("https://ristorantelavitaebella.taptasty.com/");
+        LoginPage loginPage = new LoginPage(driver);
+        OrderHistory orderHistory = new OrderHistory(driver);
+
+        logger.info("Log in");
+        loginPage.loginPage();
+        logger.info("Check order history");
+        orderHistory.orderHistory();
+        }catch (Exception e) {
+            logger.error("test failed..");
+            logger.debug("debug logs....");
+            Assert.fail();
+        }
+
+        logger.debug("application logs end.......");
+        logger.info("**** finished TC_OrderHistory *****");
+
+    }
+}
